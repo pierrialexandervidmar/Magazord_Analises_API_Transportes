83fa08a7fa62d042c6a96c20cbfba9878dd17e2f
pierrialexandervidmar <pierri@hotmail.com.br>
2024-05-01 17:25:12 -0300

    Novo frontend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,27 +1,23 @@
-//import './App.css';
-import Navbar from './components/layout/navbar';
-import CalculoAnalises from './pages/CalculoAnalises';
-import Home from './pages/Home';
-import MultiplosClientes from './pages/MultiplosClientes';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/multiplosclientes">
-            <MultiplosClientes />
-          </Route>
-          <Route path="/calculosanalises">
-            <CalculoAnalises />
-          </Route>
-        </Switch>
-      </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
